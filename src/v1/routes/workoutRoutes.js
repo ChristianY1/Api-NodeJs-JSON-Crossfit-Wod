1bf664525e711ba4c29d4db6312f11265acc11fc
Christian Yunga <christianyunga@gmail.com>
2024-03-17 12:25:43 -0500

    CRUD compleado

---
+++ src/v1/routes/workoutRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const workoutController = require("../../controllers/workoutController");
+
+router
+  .get("/", workoutController.getAllWorkouts)
+
+  .get("/:workoutId", workoutController.getOneWorkout)
+
+  .post("/", workoutController.createNewWorkout)
+
+  .put("/:workoutId", workoutController.updateOneWorkout)
+
+  .delete("/:workoutId", workoutController.deleteOneWorkout);
+
+module.exports = router;
